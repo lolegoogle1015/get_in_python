ed291026a5c110e0c727c7dce818e71b56b94d3f
lolegoogle <hryshchuk1998@gmail.com>
2020-04-01 02:02:34 +0300

    Completed program

--- projects/quadratic_equation.py
+++ projects/quadratic_equation.py
@@ -3,7 +3,25 @@
 ###На вхід програмі подаються коефіцієнти a b c
 ####В консоль повинні виводитись два корені квадратного рівняння
 import sys
+from math import sqrt
 
 a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = int(sys.argv[3])
+
+
+#a * x^2 + b * x + c = 0
+
+D = pow(b, 2) - (4*a*c)
+
+if D > 0:
+    xa = (-b + sqrt(D))/(2*a)
+    xb = (-b - sqrt(D))/(2*a)
+    print(xa)
+    print(xb)
+elif D == 0:
+    xa = xb = -(b/(2*a))
+    print(xa)
+    print(xb)
+else:
+    print("There are no roots")
