93fd49d79119df395cea8036ca330458cfc2065c
lolegoogle <hryshchuk1998@gmail.com>
2020-04-01 01:24:21 +0300

    The better v of the program

--- projects/ladder.py
+++ projects/ladder.py
@@ -6,7 +6,5 @@ import sys
 
 steps = int(sys.argv[1])
 
-for i in range(steps+1):
-    if(i == 0):
-        continue
-    print((" "*(steps-i))+"#"*i)
+for i in range(steps):
+    print(" " * (steps - i - 1), '#' * (i+1), sep="")
