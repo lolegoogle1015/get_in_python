7cff206688cb72a02d45dfa5e5173362a66a1a15
lolegoogle <hryshchuk1998@gmail.com>
2020-04-01 12:15:36 +0300

    the same program, but it uses collections.Counter

--- codewars/count_characters.py
+++ codewars/count_characters.py
@@ -1,9 +1,8 @@
 #Основна ідея - порахувати всі символи(UTF-8) у строці
 ##Наприклад стрічка 'aba' return { 'a':2 'b': 1}
 ###Якщо стрічка пуста - return {}
+from collections import Counter
 def count(string):
-    if len(string) == 0:
-        return {}
-    return {i: string.count(i) for i in string }
+     return Counter(string)
 
-print(count('aa'))
+print(count('aaasdasdaagsfhdfgafasd'))
