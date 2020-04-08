c23d56607ac0ca17b434b06658da8d266e312c7f
lolegoogle <hryshchuk1998@gmail.com>
2020-04-08 23:35:39 +0300

    short options

--- playground/coursera/prog.py
+++ playground/coursera/prog.py
@@ -1,6 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help = "display a square of a given number", type=int)
+parser.add_argument("-v", "--verbosity", help = "increase output verbosity", action="store_true")
+
 args = parser.parse_args()
-print(args.square**2)
+if args.verbosity:
+    print("verbosity turned on")
