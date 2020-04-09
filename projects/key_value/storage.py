be3c09664fe3d1e5ce5cb147067dff20eea0de1f
lolegoogle <hryshchuk1998@gmail.com>
2020-04-10 00:07:46 +0300

    Now we can parse arguments from the terminal

--- projects/key_value/storage.py
+++ projects/key_value/storage.py
@@ -1,6 +1,12 @@
 import os
 import tempfile
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-k", "--key",  action="store")
+parser.add_argument("-v", "--value", action="store", nargs='*')
+args = parser.parse_args()
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-print(storage_path)
-#with open(storage_path, 'w'):
+
+#with open(storage_path, 'w') as f:
