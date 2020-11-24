bd7724853794a273dc9e2150f6deb0176e120899
lolegoogle <hryshchuk1998@gmail.com>
2020-11-24 21:26:36 +0200

    Have added class_based views(PostListViews)

--- python_tutorial/django_tutorial/mysite/blog/views.py
+++ python_tutorial/django_tutorial/mysite/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
-from django.core.paginator import Paginator, EmptyPage, \
-                                  PageNotAnInteger
 
-# Create your views here.
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
@@ -12,16 +12,16 @@ def post_list(request):
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        #IF page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        #If pagge is out of range deliver last page of results
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-
     return render(request,
-                  'blog/post/list.html',
-                  {'page':page,
-                  'posts':posts})
+                 'blog/post/list.html',
+                 {'page': page,
+                  'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -31,4 +31,11 @@ def post_detail(request, year, month, day, post):
                                    publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
